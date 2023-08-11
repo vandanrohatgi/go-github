a8a0364d8496072358099b70dc69312973e44803
vandanrohatgi <vandanrohatgi@gmail.com>
2023-08-11 09:26:57 +0530

    feat: add method to fetch sboms

--- github/repos.go
+++ github/repos.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 )
 
 const githubBranchNotProtected string = "Branch not protected"
@@ -2049,4 +2050,66 @@ func (s *RepositoriesService) Dispatch(ctx context.Context, owner, repo string, 
 func isBranchNotProtected(err error) bool {
 	errorResponse, ok := err.(*ErrorResponse)
 	return ok && errorResponse.Message == githubBranchNotProtected
+}
+
+// SBOM stands for software bill of materials, which descibes the
+// packages/libraries that a repository depends on.
+//
+// SPDX is an open standard for software bill of materials (SBOM) that
+// identifies and catalogs components, licenses, copyrights, security
+// references, and other metadata relating to software
+type Sbom struct {
+	Sbom *SbomInfo `json:"sbom,omitempty"`
+}
+
+// When was the SBOM created
+// and who created it
+type CreationInfo struct {
+	Created  *time.Time `json:"created,omitempty"`
+	Creators []*string  `json:"creators,omitempty"`
+}
+
+type RepoDependencies struct {
+	Spdxid           *string `json:"SPDXID,omitempty"`
+	Name             *string `json:"name,omitempty"`
+	VersionInfo      *string `json:"versionInfo,omitempty"`
+	DownloadLocation *string `json:"downloadLocation,omitempty"`
+	FilesAnalyzed    *bool   `json:"filesAnalyzed,omitempty"`
+	LicenseConcluded *string `json:"licenseConcluded,omitempty"`
+	LicenseDeclared  *string `json:"licenseDeclared,omitempty"`
+}
+
+type SbomInfo struct {
+	Spdxid            *string             `json:"SPDXID,omitempty"`
+	SpdxVersion       *string             `json:"spdxVersion,omitempty"`
+	CreationInfo      *CreationInfo       `json:"creationInfo,omitempty"`
+	Name              *string             `json:"name,omitempty"`
+	DataLicense       *string             `json:"dataLicense,omitempty"`
+	DocumentDescribes []*string           `json:"documentDescribes,omitempty"`
+	DocumentNamespace *string             `json:"documentNamespace,omitempty"`
+	Packages          []*RepoDependencies `json:"packages,omitempty"`
+}
+
+func (s Sbom) String() string {
+	return Stringify(s)
+}
+
+// GetSbom fetches the Software bill of materials for a repository.
+//
+// GitHub API docs: https://docs.github.com/en/rest/dependency-graph/sboms
+func (s *RepositoriesService) GetSbom(ctx context.Context, owner string, repo string) (*Sbom, *Response, error) {
+	u := fmt.Sprintf("repos/%v/%v/dependency-graph/sbom", owner, repo)
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var sbom *Sbom
+	resp, err := s.client.Do(ctx, req, &sbom)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return sbom, resp, nil
 }
